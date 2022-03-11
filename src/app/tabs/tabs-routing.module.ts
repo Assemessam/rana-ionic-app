2214cee1acefeb8727f1dcbd8b6fc29aa7b8569c
Assemessam <55061109+Assemessam@users.noreply.github.com>
2022-03-11 18:07:53 +0200

    implementing the details page design

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,15 +9,18 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        children:[{
+        children:[
+          {
           path:'',
           loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
 
-        },{
-          path: 'activity-detail', 
+        },
+        {
+          path: 'activity-detail/:activityID', 
           loadChildren: () => import('../activity-detail/activity-detail.module').then(m => m.ActivityDetailPageModule)
 
-        }]
+        }
+      ]
       },
       {
         path: 'tab2',
